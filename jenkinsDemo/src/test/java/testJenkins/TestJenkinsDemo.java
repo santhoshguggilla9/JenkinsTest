22d05ef08d9a8f55f8b9a913e79952ddd35c051e
santh <santh@SanPC>
2024-02-29 21:39:09 +0530

    commit

--- jenkinsDemo/src/test/java/testJenkins/TestJenkinsDemo.java
+++ jenkinsDemo/src/test/java/testJenkins/TestJenkinsDemo.java
@@ -2,19 +2,38 @@ package testJenkins;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TestJenkinsDemo {
 
-	WebDriver driver;
+	WebDriver driver=null;
+	@Parameters("browser")
 	@BeforeClass
-	public void setupBrowser()
+	public void setupBrowser(String browser)
 	{
-		driver=new ChromeDriver();
-		driver.manage().window().maximize();
+		if(browser.equalsIgnoreCase("edge"))
+		{
+			driver=new EdgeDriver();
+			driver.manage().window().maximize();
+		}
+		else if(browser.equalsIgnoreCase("chrome"))
+		{
+			driver=new ChromeDriver();
+			driver.manage().window().maximize();
+		}
+		else if(browser.equalsIgnoreCase("firefox"))
+		{
+			driver=new FirefoxDriver();
+			driver.manage().window().maximize();
+		}
+		
 	}
+	
 	@Test
 	public void openBrowser()
 	{
